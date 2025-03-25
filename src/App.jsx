8917876d088c7350d9d43ad39602c885301cfbc5
Tobe Okafor <teokafor@gmail.com>
2025-03-24 21:01:14 -0500

    Switch to DragOverlay solution
    Tosses out what little work I had (hence the new branch.) Done to get return animations on card discard.

--- src/App.jsx
+++ src/App.jsx
@@ -1,45 +1,42 @@
 import React, {useState} from 'react';
-import {DndContext} from '@dnd-kit/core';
+import {DndContext, DragOverlay} from '@dnd-kit/core';
 
 import { Draggable } from './Components/Draggable';
 import { GridCell } from './Components/GridCell';
 
 
+import { Item } from './Item';
+
 // Build an empty array to represent grid cells
 const containers = Array.apply(null, Array(25)).map(function (x, i) { return 'grid-droppable-' + i; });
 
 function App() {
-  const [cardAParent, setCardAParent] = useState(null);
-  const [cardBParent, setCardBParent] = useState(null);
-  const cardA = (<Draggable id='active-card-a'>Card A</Draggable>);
-  const cardB = (<Draggable id='active-card-b'>Card B</Draggable>);
-  
+  const [items] = useState(['1', '2', '3', '4', '5']);
+  const [activeId, setActiveId] = useState(null);
+
   return (
-    <DndContext onDragEnd={handleDragEnd}>
-      {/* Move the card to the top if not related to a grid cell. */}
-      {cardAParent === null ? cardA : null} 
-      {cardBParent === null ? cardB : null}
-      {
-        <div className="grid-bg">
-        {containers.map((id) => (
-          <GridCell id={id} key={id}>
-            {/* Add draggable to matching element from drag end. */}
-            {cardAParent === id ? cardA : null} 
-            {cardBParent === id ? cardB : null}
-          </GridCell>
-          ))}
-        </div>
-      }
-      
+    <DndContext onDragStart={handleDragStart} onDragEnd={handleDragEnd}>
+    {items.map(id =>
+          <Draggable key={id} id={id}>
+            {activeId === id ? <>{`Item ${id} SELECTED`}</> : <>{`Item ${id} BASESTATE`}</>}
+          </Draggable>
+    )}
+
+
+    <DragOverlay>
+        {activeId ? (
+            <Item value={`Item ${activeId} DRAGSTATE`} /> 
+        ): null}
+        </DragOverlay>
     </DndContext>
   );
   
-  function handleDragEnd(event) {
-    const {over} = event;
-    const cardId = event.active.id;
 
-    if (cardId === 'active-card-a') setCardAParent(over ? over.id : null);
-    else setCardBParent(over ? over.id : null);
+  function handleDragStart(event) {
+    setActiveId(event.active.id);
+  }
+  function handleDragEnd() {
+    setActiveId(null);
   }
 }
 
--- src/Components/Draggable.jsx
+++ src/Components/Draggable.jsx
@@ -2,17 +2,13 @@ import React from 'react';
 import {useDraggable} from '@dnd-kit/core';
 
 export function Draggable(props) {
-  const {attributes, listeners, setNodeRef, transform} = useDraggable({
+  const {attributes, listeners, setNodeRef} = useDraggable({
     id: props.id,
   });
-  const style = transform ? {
-    transform: `translate3d(${transform.x}px, ${transform.y}px, 0)`,
-  } : undefined;
-
   
   return (
-    <button ref={setNodeRef} style={style} {...listeners} {...attributes}>
+    <li ref={setNodeRef} {...listeners} {...attributes}>
       {props.children}
-    </button>
+    </li>
   );
 }
