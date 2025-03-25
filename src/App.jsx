0cf1b913d01a981ba5e3fec59a741313ed36c7ea
Tobe Okafor <teokafor@gmail.com>
2025-03-24 20:06:30 -0500

    Add second card draggable

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,6 @@ import React, {useState} from 'react';
 import {DndContext} from '@dnd-kit/core';
 
 import { Draggable } from './Components/Draggable';
-import { Droppable } from './Components/Droppable';
 import { GridCell } from './Components/GridCell';
 
 
@@ -10,21 +9,23 @@ import { GridCell } from './Components/GridCell';
 const containers = Array.apply(null, Array(25)).map(function (x, i) { return 'grid-droppable-' + i; });
 
 function App() {
-  const [parent, setParent] = useState(null);
-
-  const draggableMarkup = (
-    <Draggable id='draggable'>Drag me</Draggable>
-  );
+  const [cardAParent, setCardAParent] = useState(null);
+  const [cardBParent, setCardBParent] = useState(null);
+  const cardA = (<Draggable id='active-card-a'>Card A</Draggable>);
+  const cardB = (<Draggable id='active-card-b'>Card B</Draggable>);
   
   return (
     <DndContext onDragEnd={handleDragEnd}>
-      {parent === null ? draggableMarkup : null}
-
+      {/* Move the card to the top if not related to a grid cell. */}
+      {cardAParent === null ? cardA : null} 
+      {cardBParent === null ? cardB : null}
       {
         <div className="grid-bg">
         {containers.map((id) => (
           <GridCell id={id} key={id}>
-            {parent === id ? draggableMarkup : 'test'}
+            {/* Add draggable to matching element from drag end. */}
+            {cardAParent === id ? cardA : null} 
+            {cardBParent === id ? cardB : null}
           </GridCell>
           ))}
         </div>
@@ -35,7 +36,10 @@ function App() {
   
   function handleDragEnd(event) {
     const {over} = event;
-    setParent(over ? over.id : null);
+    const cardId = event.active.id;
+
+    if (cardId === 'active-card-a') setCardAParent(over ? over.id : null);
+    else setCardBParent(over ? over.id : null);
   }
 }
 
