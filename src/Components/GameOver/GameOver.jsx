b61b47def3d427ecf7069cfc8909ec414f42b7f2
Tobe Okafor <teokafor@gmail.com>
2025-04-09 00:22:06 -0500

    Add new high score animation

--- src/Components/GameOver/GameOver.jsx
+++ src/Components/GameOver/GameOver.jsx
@@ -2,8 +2,25 @@ export function GameOver({func, isNewHighScore}) {
     return (
         <div className='game-over-screen'>
             <div className='header-text'>GAME OVER</div>
-            {isNewHighScore ? <div className='new-high'>NEW HIGH SCORE</div> : null }
+            {isNewHighScore ? <div className='new-high'>
+                 <span>N</span>
+                 <span>E</span>
+                 <span>W</span>
+                 <span>&nbsp;</span>
+                 <span>H</span>
+                 <span>I</span>
+                 <span>G</span>
+                 <span>H</span>
+                 <span>&nbsp;</span>
+                 <span>S</span>
+                 <span>C</span>
+                 <span>O</span>
+                 <span>R</span>
+                 <span>E</span>
+                </div> : null }
             <button onClick={func} className='return-button'>PLAY AGAIN</button>
         </div>
     );
 }
+
+// NEW HIGH SCORE
